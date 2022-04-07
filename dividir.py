3f5e215029b7d2414ea3823422d8c41f7bf52c52
hitman472005 <103201022+hitman472005@users.noreply.github.com>
2022-04-07 11:33:57 -0400

    Add files via upload

---
+++ dividir.py
@@ -0,0 +1,6 @@
+d1 = int(input("Introdusca el primer numero divisor: "))
+d2 = int(input("Introdusca el segundo numero que dividira: "))
+
+resultado = d1 / d2
+
+print(f"La multiplicacion es igual a: {resultado}")
